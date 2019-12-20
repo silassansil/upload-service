c1ac652f009de8c60f83652124706e513de0cf1c
ssantos <ssantos@avenuecode.com>
2019-12-19 22:28:04 -0300

    interface javadoc

--- src/main/java/br/com/challenge/uploadservice/service/ImageService.java
+++ src/main/java/br/com/challenge/uploadservice/service/ImageService.java
@@ -6,13 +6,45 @@ import java.util.Collection;
 
 public interface ImageService {
 
+    /**
+     * Upload image to customer id
+     *
+     * @param image image data
+     * @return image saved
+     */
     Image uploadImage(final Image image);
 
+    /**
+     * Find by customer id
+     *
+     * @param customerId customer id
+     * @return collection with all images owned customers
+     */
     Collection<Image> findByCustomerId(final String customerId);
 
+    /**
+     * Find by id and customer id
+     *
+     * @param customerId customer id
+     * @param id         image id
+     * @return images owned by customer id
+     */
     Image findByIdAndCustomerId(final String customerId, final String id);
 
+    /**
+     * Find by customer id and name
+     *
+     * @param customerId customer id
+     * @param name       name
+     * @return collection with all images owned customers and with name
+     */
     Collection<Image> findByCustomerIdAndName(final String customerId, final String name);
 
+    /**
+     * Delete by id and customer id
+     *
+     * @param customerId customer id
+     * @param id         image id
+     */
     void deleteByIdAndCustomerId(final String customerId, final String id);
 }
